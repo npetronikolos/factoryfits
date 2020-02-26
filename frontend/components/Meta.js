805427eab0a6b253ed2d88165b2ebe14626c720d
npetronikolos <petronikolosn@gmail.com>
2020-02-26 19:48:47 +0100

    some small additions to Meta,Header,_document

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
+import NextHead from "next/head";
 
 const Meta = () => (
-  <Head>
+  <NextHead>
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <meta charSet='utf-8' />
     <link rel='shortcut icon' href='/static/favicon.png' />
     <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
     <title>Factory Fits!</title>
-  </Head>
+  </NextHead>
 );
 
 export default Meta;
